438178768d547b395127ce7d85db669eab1ee714
SaeediAlireza <alirexasaeedi@gmail.com>
2024-08-14 15:45:28 +0330

    fixing CROS

--- main.py
+++ main.py
@@ -22,10 +22,21 @@ import routers.transaction
 import routers.user
 import routers.user_type
 import routers.weight_class
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 model.Base.metadata.create_all(database.engine)
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routers.ages.router)
 app.include_router(routers.authentication.router)
